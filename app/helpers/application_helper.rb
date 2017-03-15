91ae471ca003806b1414eaaaffdbc91cd4fcde3c
grek-andrian <gretsko.andrian@gmail.com>
2017-03-15 21:18:57 +0200

    Add flash and style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
-    def active_page(active_page)
-        @active == active_page ? 'active' : ''
-   end
+  def flash_class(level)
+     case level.to_sym
+     when :notice then "alert alert-info"
+     when :success then "alert alert-success"
+     when :error then "alert alert-error"
+     when :alert then "alert alert-error"
+     end
+  end
+
+  def active_page(active_page)
+     @active == active_page ? 'active' : ''
+  end
 end
