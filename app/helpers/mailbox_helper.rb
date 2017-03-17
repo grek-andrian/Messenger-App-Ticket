bca5919fd1d1b98db81413d6e550f978f64f5dfc
grek-andrian <gretsko.andrian@gmail.com>
2017-03-17 13:10:17 +0200

    Add avatar and config nav panel

--- app/helpers/mailbox_helper.rb
+++ app/helpers/mailbox_helper.rb
@@ -1,5 +1,5 @@
 module MailboxHelper
     def unread_messages_count
-        @unread_count = mailbox.inbox(read: false).count(:id, distinct: true).to_s
+        current_user.mailbox.inbox(read: false).count
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :trackable, :validatable
 
+    #Validation for avatar
+    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+    #Setting for mailboxer
     acts_as_messageable
 
     def mailboxer_name
