a18677be7633766bdb80383e4dc129b4f6cab2fa
grek-andrian <gretsko.andrian@gmail.com>
2017-03-18 23:03:20 +0200

    Create main page with links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-    devise_for :users
+    devise_for :users, path_names: {
+        sign_up: ''
+    }
     resources :users_admin, controller: 'users'
-    
+
     root 'welcome#index'
     # mailbox folder routes
     get 'mailbox/inbox', to:  'mailbox#inbox', as: :mailbox_inbox
